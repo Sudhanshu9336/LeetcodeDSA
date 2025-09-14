e827a5b276aafb834a8862e854c3dc1ec43f2b4b
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-09-14 15:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.84%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double p=pow(x,n);
-        return p;
+        long long m = n;           // ✅ double -> long long
+        long long exp = abs(m);    // ✅ safe abs for INT_MIN
+        double ans = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                ans = ans * x;
+                exp = exp - 1;
+            } else {
+                exp = exp / 2;
+                x = x * x;
+            }
+        }
+        if (m < 0) return 1 / ans;
+        return ans;
     }
 };
