8b6d81701d694646525c20d852ea62bd67ee2098
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-09-16 01:33:33 +0530

    Time: 2 ms (57.55%) | Memory: 32.7 MB (39.05%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        deque<int>q;
-        int l=0,r=0,sum=0,count=0;
-        while(r<nums.size())
-        {
-            sum+=nums[r];
-            while(sum>goal)
-            {
-                sum-=nums[l];
-                l++;
-            }
-            count+=(r-l+1);
-            r++;
+int subarray(vector<int>&nums,int goal){
+    int count=0;
+    int r=0;
+    int sum=0;
+    int l=0;
+    while(r<nums.size()){
+        if(goal<0)return 0;
+        sum=sum+nums[r];
+        while(sum>goal){
+            sum=sum-nums[l];
+            l++;
         }
-        goal--;
-        int cnt=0;
-        l=0;
-        r=0;
-        sum=0;
-        if(goal>=0){
-        while(r<nums.size())
-        {
-            sum+=nums[r];
-            while(sum>goal)
-            {
-                sum-=nums[l];
-                l++;
-            }
-            cnt+=(r-l+1);
-            r++;
-        }}
-        return count-cnt;
+        count=count+(r-l+1);
+        r++;
+       
+    }
+     return count;
+}
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int result=subarray(nums,goal)-subarray(nums,goal-1);
+        return result;
     }
 };
