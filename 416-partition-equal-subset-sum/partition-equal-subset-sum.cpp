802fc10495fcc57eb76b33bf4968b6209eff1c18
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-09-21 20:23:33 +0530

    Time: 194 ms (12.66%) | Memory: 204.1 MB (7.89%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+bool solve(int index,vector<int>&nums,int target,vector<vector<int>>&dp){
+       if(target==0)return true;
+       if(index>=nums.size())return false;
+       if(dp[index][target]!=-1) return dp[index][target];
+   
+    bool take=false;
+    if(nums[index]<=target){
+     take=solve(index+1,nums,target-nums[index],dp);
+}
+    bool nottake=solve(index+1,nums,target,dp);
+    return dp[index][target]=(take||nottake);
+
+    }
+
+    bool canPartition(vector<int>& nums){
+        int n=nums.size();
+        int sum=0;
+        for(auto val:nums){
+            sum=sum+val;
+        }
+        if(sum%2!=0)return false;
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return solve(0,nums,sum/2,dp);
+    }
+};
+
+//recursion time limit maar dega marega 
+// class Solution {
+// public:
+// bool solve(int index,vector<int>&nums,int target){
+//        if(target==0)return true;
+//       if(index==nums.size()-1){
+//         return nums[index]==target;
+//     }
+//     bool take=false;
+//     if(nums[index]<=target){
+//      take=solve(index+1,nums,target-nums[index]);
+// }
+//     bool nottake=solve(index+1,nums,target);
+//     return take||nottake;
+
+//     }
+
+//     bool canPartition(vector<int>& nums){
+//         int n=nums.size();
+//         int sum=0;
+//         for(auto val:nums){
+//             sum=sum+val;
+//         }
+//         if(sum%2!=0)return false;
+//         return solve(0,nums,sum/2);
+//     }
+// };
