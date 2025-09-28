2b3a8015038a9f0dfd46d8a6804322c0d3d5333b
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-09-28 17:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (27.52%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,32 +1,32 @@
 
-// //poora row mujhe pascals triangle banane ki jarurat nahi hai mujhe
-// class Solution {
-// public:
-//     vector<int> getRow(int rowIndex) {
-//        vector<vector<int>>result;
-//         int n=rowIndex;
-//         for(int i=0;i<=n;i++){
-//           vector<int>ans(i+1,1);
-//           for(int j=1;j<i;j++){
-//             ans[j]=result[i-1][j-1]+result[i-1][j];
-//           }
-//           result.push_back(ans);
-//         }
-//         return result[n];
-        
-//     }
-// };
-
-
+//poora row mujhe pascals triangle banane ki jarurat nahi hai mujhe
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> row(rowIndex + 1, 1);
-        for (int i = 1; i < rowIndex; i++) {
-            for (int j = i; j > 0; j--) {
-                row[j] = row[j] + row[j-1];
-            }
+       vector<vector<int>>result;
+        int n=rowIndex;
+        for(int i=0;i<=n;i++){
+          vector<int>ans(i+1,1);
+          for(int j=1;j<i;j++){
+            ans[j]=result[i-1][j-1]+result[i-1][j];
+          }
+          result.push_back(ans);
         }
-        return row;
+        return result[n];
+        
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> getRow(int rowIndex) {
+//         vector<int> row(rowIndex + 1, 1);
+//         for (int i = 1; i < rowIndex; i++) {
+//             for (int j = i; j > 0; j--) {
+//                 row[j] = row[j] + row[j-1];
+//             }
+//         }
+//         return row;
+//     }
+// };
