f420151ccc0eef87b9a08c2116fe08736d4b642b
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-10-06 02:03:07 +0530

    Time: 1 ms (54.49%) | Memory: 10.7 MB (33.38%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+   bool isvowel(char ch){
+    switch(ch){
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+     case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    return true;
+    default :
+    return false;
+}
+}
+    string reverseVowels(string s) {
+        vector<char>vowel;
+        for(auto ch:s){
+            if(isvowel(ch)){
+                vowel.push_back(ch);
+            }
+        }
+        int index=vowel.size()-1;
+        for(auto &ch:s){
+            if(isvowel(ch)){
+                ch=vowel[index];
+                index--;
+            }
+        }
+        return s;
+    }
+};
