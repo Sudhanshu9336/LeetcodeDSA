bab17048d27e641ae0c9df1fdcfa6b29ccbf72f0
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-10-11 18:19:21 +0530

    Time: 21 ms (64.64%) | Memory: 85.7 MB (38.14%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int area = 0;
+        int n = heights.size();
+        stack<int> st;
+        vector<int> rightsmallerindex(n);
+        vector<int> leftsmallerindex(n);
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                st.push(i);
+                rightsmallerindex[i] = n;
+            } else {
+               rightsmallerindex[i] = st.top();
+            }
+                st.push(i);
+        }
+        while (!st.empty()){
+            st.pop();
+        }
+        for (int i = 0; i < n; i++) {
+              while (!st.empty()&&heights[st.top()] >=heights[i]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                leftsmallerindex[i] = -1;
+            }
+            else {
+                  leftsmallerindex[i]=st.top();  
+            }
+            st.push(i);
+        }
+
+        for (int i = 0; i < heights.size(); i++) {
+            area = max(area, heights[i] * (rightsmallerindex[i]-leftsmallerindex[i]-1));
+        }
+        return area;
+    }
+};
+
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& heights) {
+//         int n=heights.size();
+//         int maxarea=0;
+//         for(int i=0;i<n;i++){
+//             int minheight=heights[i];
+//             for(int j=i;j<n;j++){
+//                 minheight=min(minheight,heights[j]);
+//                 int width=j-i+1;
+//                 int area=width*minheight;
+//                 maxarea=max(maxarea,area);
+//             }
+//         }
+//         return maxarea;
+//     }
+// };
