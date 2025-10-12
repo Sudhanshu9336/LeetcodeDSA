e087b5aa06a9596315f7bbbd54ec47407396c04e
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-10-12 11:26:27 +0530

    Time: 22 ms (58.49%) | Memory: 102.7 MB (91.25%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int>st;
+        int n=temperatures.size();
+        vector<int>ans(n,0);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty()&&temperatures[st.top()]<=temperatures[i]){
+                st.pop();
+            }
+             if(!st.empty()&&temperatures[st.top()]>temperatures[i]){
+                ans[i]=st.top()-i;
+            }
+          
+            st.push(i);
+        }
+        return ans;
+    }
+};
