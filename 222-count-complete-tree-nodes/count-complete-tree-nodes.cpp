722d9ce478ee8d73f8b9d5a2ac31a5921573708e
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-10-14 02:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.33%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -9,19 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-int ans=0;
-int count(TreeNode* root){
-    if(root==NULL)return 0;
-    int left=count(root->left);
-    int right=count(root->right);
-     return ans = 1+(left+right);
+// class Solution {
+// public:
+// int ans=0;
+// int count(TreeNode* root){
+//     if(root==NULL)return 0;
+//     int left=count(root->left);
+//     int right=count(root->right);
+//      return ans = 1+(left+right);
     
 
-}
+// }
+//     int countNodes(TreeNode* root) {
+//         count(root);
+//         return ans;
+//     }
+// };
+class Solution {
+public:
+    int count(TreeNode* root) {
+        if (root == NULL) return 0;
+        return 1 + count(root->left) + count(root->right);
+    }
+
     int countNodes(TreeNode* root) {
-        count(root);
-        return ans;
+        return count(root);
     }
 };
