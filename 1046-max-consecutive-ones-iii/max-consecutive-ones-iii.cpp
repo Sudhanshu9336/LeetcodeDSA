343de62f4daf2a11ce96a787e5def0437f1588d7
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-10-30 23:40:38 +0530

    Time: 0 ms (100.00%) | Memory: 69.7 MB (25.94%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,48 +1,70 @@
-// class Solution {
-// public:
-//     int longestOnes(vector<int>& nums, int k) {
-//         int maxl=0;
-//         int n=nums.size();
-//         for(int i=0;i<n;i++){
-//             int count=0;
-//             for(int j=i;j<n;j++){
-//                 if(nums[j]!=1)count++;
-//                 if(count>k)break;
-//                 int le=j-i+1;
-//                 maxl=max(maxl,le);
-//             }
-//         }
-//         return maxl;
-//     }
-// };
-
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
+        int n=nums.size();
         int l=0;
         int r=0;
         int count=0;
-        int maxi=INT_MIN;
-        while(r<nums.size()){
-            
-             if(nums[r]==0 ){
+        int maxl=INT_MIN;
+        while(r<n){
+            if(nums[r]==0){
                 count++;
-             }
-                while(count>k){
-                    if(nums[l]==0){
-                    
-                     count--;
-                    }
-                    l++;
+            }
+            while(count>k){
+                if(nums[l]==0){
+                    count--;
                 }
-            
-            
-                maxi=max(maxi , r-l+1);
-                
-            
-             r++;
+                l++;
+            }
+            maxl=max(maxl,r-l+1);
+            r++;
         }
-        return maxi;
-        
+        return maxl;
+
     }
 };
+// // class Solution {
+// // public:
+// //     int longestOnes(vector<int>& nums, int k) {
+// //         int maxl=0;
+// //         int n=nums.size();
+// //         for(int i=0;i<n;i++){
+// //             int count=0;
+// //             for(int j=i;j<n;j++){
+// //                 if(nums[j]!=1)count++;
+// //                 if(count>k)break;
+// //                 int le=j-i+1;
+// //                 maxl=max(maxl,le);
+// //             }
+// //         }
+// //         return maxl;
+// //     }
+// // };
+
+// class Solution {
+// public:
+//     int longestOnes(vector<int>& nums, int k) {
+//         int l=0;
+//         int r=0;
+//         int count=0;
+//         int maxi=INT_MIN;
+//         while(r<nums.size()){
+            
+//              if(nums[r]==0 ){
+//                 count++;
+//              }
+//                 while(count>k){
+//                     if(nums[l]==0){
+                    
+//                      count--;
+//                     }
+//                     l++;
+//                 }
+//                 maxi=max(maxi , r-l+1);
+
+//              r++;
+//         }
+//         return maxi;
+        
+//     }
+// };
