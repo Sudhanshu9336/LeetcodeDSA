54a16e592f098891633bdf4cf4c41f5ae5eb9f30
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-11-16 23:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (69.91%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        set<int>set(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=0;i<=n;i++){
-            if(set.find(i)==set.end()){
-                return i;
+        int n=0;
+        int p=0;
+        for(int i=0;i<=nums.size();i++){
+            n=n^i;
+            if(i<nums.size()){
+                 p=nums[i]^p;
             }
+           
         }
-        return -1;
+        return p^n;
+    
     }
 };
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         set<int>set(nums.begin(),nums.end());
+//         int n=nums.size();
+//         for(int i=0;i<=n;i++){
+//             if(set.find(i)==set.end()){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
