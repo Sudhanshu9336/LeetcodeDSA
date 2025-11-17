826d93b32b9b2215d1960da4caa0d4a0ea42b78f
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-11-17 23:47:10 +0530

    Time: 3 ms (68.72%) | Memory: 14.9 MB (36.49%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,11 +5,15 @@ public:
         int t=target;
         for(int i=0;i<nums.size();i++){
             t=target;
-            for(auto val:mapi){
-                  int reman=t-nums[i];
-                if(val.first==reman){
-                    return {i,val.second};
-                }
+            // for(auto val:mapi){
+            //       int reman=t-nums[i];
+            //     if(val.first==reman){
+            //         return {i,val.second};
+            //     }
+            // }
+            int rem=t-nums[i];
+            if(mapi.count(rem)){
+                return {i,mapi[rem]};
             }
             mapi[nums[i]]=i;
         }
