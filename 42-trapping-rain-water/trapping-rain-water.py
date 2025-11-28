434c6c4eca41f8e38c7cf58a3b99511d0cc38a9f
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-11-28 15:32:22 +0530

    Time: 27 ms (31.70%) | Memory: 14.2 MB (14.27%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -11,17 +11,16 @@ class Solution(object):
         left = [0] * n
         right = [0] * n
 
-        # Left max array
+    
         left[0] = height[0]
         for i in range(1, n):
             left[i] = max(left[i - 1], height[i])
 
-        # Right max array
+      
         right[n - 1] = height[n - 1]
         for i in range(n - 2, -1, -1):
             right[i] = max(right[i + 1], height[i])
 
-        # Calculate trapped water
         ans = 0
         for i in range(n):
             ans += min(left[i], right[i]) - height[i]
