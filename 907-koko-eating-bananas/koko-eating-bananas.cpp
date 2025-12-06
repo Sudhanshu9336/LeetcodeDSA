c529eaa0baa29257af4336d82d6fae4343ce1b6d
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2025-12-06 19:02:48 +0530

    Time: 18 ms (13.05%) | Memory: 22.9 MB (77.26%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            long long hours = 0;
-            for (int pile : piles) {
-                hours =hours+ceil((double)pile/mid);
+        int left=1;
+        int right=*max_element(piles.begin(),piles.end());
+       
+        while(left<right){
+            int mid=left+(right-left)/2;
+            long long  hour=0;
+            for(auto pile:piles){
+                 hour =hour+ceil((double)pile/mid);
             }
-            if (hours <= h) {
-                right = mid; 
-            } else {
-                left = mid + 1; 
+            if(hour<=h){
+                right=mid;
+            }
+            else {
+                left=mid+1;
             }
         }
-        
         return left;
+        
     }
 };
