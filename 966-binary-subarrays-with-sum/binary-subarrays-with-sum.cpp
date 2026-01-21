5aeb00b724bf816918243294e5fa6ab02c3cfe3f
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-01-21 23:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (81.23%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-int subarray(vector<int>&nums,int goal){
-    int count=0;
-    int r=0;
-    int sum=0;
+int findingsum(vector<int>&nums,int g){
+    int presum=0;
     int l=0;
+    int r=0;
+    int count=0;
     while(r<nums.size()){
-        if(goal<0)return 0;
-        sum=sum+nums[r];
-        while(sum>goal){
-            sum=sum-nums[l];
-            l++;
-        }
-        count=count+(r-l+1);
-        r++;
-       
+        if(g<0)return 0;
+        presum=presum+nums[r];
+       while(presum>g){
+        presum=presum-nums[l];
+        l++;
+       }
+       count=count+(r-l+1);
+       r++;
     }
-     return count;
+    return count;
+
 }
+
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int result=subarray(nums,goal)-subarray(nums,goal-1);
-        return result;
+    int result=findingsum(nums,goal)-findingsum(nums,goal-1);
+    return result;
     }
 };
