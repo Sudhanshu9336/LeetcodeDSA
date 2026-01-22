17f8931bd1c3101b39b27d7580861d77edbb061c
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-01-22 22:05:05 +0530

    Time: 16 ms (30.96%) | Memory: 71.4 MB (70.46%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-int solve(vector<int>&nums,int  k){
+int countt(vector<int>&nums,int k){
     int l=0;
     int r=0;
-    int count=0;
     int sum=0;
+    int count=0;
     while(r<nums.size()){
-       sum=sum+nums[r];
-       while(sum>k)
-       {
-        sum=sum-nums[l];
-        l++;
-       }
-       count=count+r-l+1;
-       r++;
+        sum=sum+nums[r];
+        while(sum>k){
+            sum=sum-nums[l];
+            l++;
+        }
+        count=count+(r-l+1);
+        r++;
     }
     return count;
 }
@@ -22,11 +21,11 @@ int solve(vector<int>&nums,int  k){
             if(nums[i]%2==0){
                 nums[i]=0;
             }
-            else {nums[i]=1;
+            else{
+                nums[i]=1;
             }
         }
-        int ans=solve(nums,k)-solve(nums,k-1);
+        int ans=countt(nums,k)-countt(nums,k-1);
         return ans;
-        
     }
 };
