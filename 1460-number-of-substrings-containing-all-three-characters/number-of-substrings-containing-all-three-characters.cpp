e5e362ea1f769a1644967c26edb6e6a6e29d7509
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-01-23 21:24:37 +0530

    Time: 5 ms (72.96%) | Memory: 11.2 MB (21.21%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n=s.size();
         int count=0;
-
-        map<char,int>map;
+        int n=s.size();
+        unordered_map<char,int>map;
         for(int i=0;i<n;i++){
-             map[s[i]]=i+1;
+            map[s[i]]=i+1;
             if(map.size()==3){
-                  int mini=INT_MAX;
+                int mini=INT_MAX;
                 for(auto it:map){
                     mini=min(it.second,mini);
                 }
-                count=count+mini;
+                    count=count+mini;
+                
             }
-       
+           
         }
-        return count;
-        
+         return count;
     }
 };
-// class Solution {
-// public:
-//     int numberOfSubstrings(string s) {
-//         int count = 0;
-//         int n = s.size();
-        
-//         for(int i = 0; i < n; i++) {
-//             vector<int> freq(3, 0);
-//             for(int j = i; j < n; j++) {
-//                 freq[s[j] - 'a'] = 1; 
-//                 if(freq[0] + freq[1] + freq[2] == 3) {
-//                     //uske aage ka bhi hum addkar sakte hai
-//                     count=count+n-j;
-//                     break;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// };
