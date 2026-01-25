0f7f1dfc63e5b3a58ce7aaf4804f7772c5a91db6
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-01-26 00:55:57 +0530

    Time: 7 ms (17.59%) | Memory: 9.7 MB (80.23%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n==1)return "1";
-        string pre=countAndSay(n-1);
-        string ans="";
-        int count=1;
-        for(int i=1;i<=pre.size();i++){
-            if(i<pre.size()&&pre[i]==pre[i-1]){
-                count++;
+        string ans = "1";
+
+        for (int step = 2; step <= n; step++) {
+            string p = "";
+            int count = 1;
+
+            for (int i = 1; i < ans.size(); i++) {
+                if (ans[i] == ans[i - 1]) {
+                    count++;
+                } else {
+                    p += to_string(count) + ans[i - 1];
+                    count = 1;
+                }
             }
-            else{
-                ans=ans+to_string(count);
-                ans=ans+pre[i-1];
-                count=1;
-            }
+
+         p += to_string(count) + ans.back();
+            ans = p;
         }
         return ans;
-
-        
     }
 };
