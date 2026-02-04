40cf050c5df911399927d64819a58e1d0a3a78f5
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-02-04 11:01:17 +0530

    Time: 1 ms (25.56%) | Memory: 9.4 MB (52.02%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-   int solve(int m,int n,vector<vector<int>>&dp){
-    if(m==0&&n==0)return 1;
-    if(m<0||n<0)return 0;
-    if(dp[m][n]!=-1)return dp[m][n];
-    int up=solve(m-1,n,dp);
-    int left=solve(m,n-1,dp);
-    return dp[m][n]=up+left;
+   int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
+  if(i==m-1&&j==n-1)return 1;
+  if(i>=m||j>=n)return 0;
+  if(dp[i][j]!=-1){
+    return dp[i][j];
+  }
+    int right = solve(i, j+1, m, n, dp);
+     int down  = solve(i+1, j, m, n, dp);
+         return dp[i][j] = right + down;
 }
+
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        int ans=solve(m-1,n-1,dp);
+        int ans=solve(0,0,m,n,dp);
         return ans;
         
     }
 };
-//class Solution {
-// public:
-// int solve(int m,int n){
-//     if(m==0&&n==0)return 1;
-//     if(m<0||n<0)return 0;
-//     int up=solve(m-1,n);
-//     int left=solve(m,n-1);
-//     return left+up;
-// }
-//     int uniquePaths(int m, int n) {
-//         int ans=solve(m-1,n-1);
-//         return ans;
-//     }
-// };
