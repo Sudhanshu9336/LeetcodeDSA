d2243ec44e3aea5a279448a3044a9b2d1d65e03e
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-03-11 02:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (45.30%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,28 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        //helper function to check is this true or false
-        return validate(root,LONG_MIN,LONG_MAX);
+        return validate(root, LONG_MIN, LONG_MAX);
     }
-    bool validate(TreeNode* node ,long minval,long maxval){
-        if(node==NULL){
+    bool validate(TreeNode* node, long minvalue, long maxvalue) {
+        if(node == NULL){
             return true;
         }
-        if(node->val>=maxval ||node->val<=minval){
+        if(node->val >= maxvalue || node->val <= minvalue){
             return false;
         }
-        //matlab jub left mai jayenegy to node->left mai jayenge aur jo min val vio min he rahegi aur maxval vo node ke current val ke barabar ho jayegi and sath sath mai rigjt ko bhi vheck karega ki jo min value hai vo node ki val ke bararabar hoga aur max value th max value rahega agar kisi mai se ek bhi false hua toh retur false ya true kar dega
-        return validate(node->left,minval,node->val) && validate(node->right,node->val,maxval);
+        return validate(node->left, minvalue, node->val) &&
+               validate(node->right, node->val, maxvalue);
     }
 };
