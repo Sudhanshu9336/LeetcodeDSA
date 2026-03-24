aa72cf9e05617016658c2bed80b151d84203244c
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-03-25 00:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (58.11%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int p=0;
-        for(int i=0;i<nums.size();i++){
-        p=p^nums[i];
-    }
-    return p;
+        for(auto x:nums){
+            p=p^x;
+        }
+        return p;
     }
 };
