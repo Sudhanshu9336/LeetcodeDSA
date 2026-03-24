82420336f92f5403baa71dd6f9710e37b0f15b2d
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-03-24 23:47:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (7.87%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+              int n=nums.size();
         unordered_map<int,int>map;
         for(auto val:nums){
             map[val]++;
         }
-        // priority_queue<pair<int,int>>pq;
-        // for(auto it:map){
-        //     pq.push({it.second,it.first});
-        // }
-        // return pq.top().second;
-        int m=nums.size()/2;
         for(auto it:map){
-            if(it.second>m){
+            if(it.second>n/2){
                 return it.first;
-      }
+            }
         }
         return -1;
+    
     }
 };
