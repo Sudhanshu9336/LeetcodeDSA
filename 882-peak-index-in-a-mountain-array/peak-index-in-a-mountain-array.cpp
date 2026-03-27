7c17789994e4ca9526a6a5cbfc2bdd44c82a982c
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-03-27 21:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 63.4 MB (78.08%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -2,11 +2,30 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n=arr.size();
-        for(int i=1;i<n-1;i++){
-            if(arr[i-1]<arr[i]&&arr[i]>arr[i+1]){
-                return i;
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            int m=l+(r-l)/2;
+            if(arr[m]<arr[m+1]){
+                l=m+1;
+            }
+            else{
+                r=m;
             }
         }
-        return -1;
+        return l;
     }
 };
+
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//         int n=arr.size();
+//         for(int i=1;i<n-1;i++){
+//             if(arr[i-1]<arr[i]&&arr[i]>arr[i+1]){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
