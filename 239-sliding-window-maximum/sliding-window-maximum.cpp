f8b60e5e73a35b2dc701cbc28ff06af1ff681187
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-03-28 11:53:23 +0530

    Time: 28 ms (39.81%) | Memory: 139.3 MB (43.76%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>q;
         int n=nums.size();
         vector<int>ans;
-        deque<int>d;
         for(int i=0;i<n;i++){
-            if(!d.empty()&&d.front()<=i-k){
-               d.pop_front();
-            }
-            while(!d.empty()&&nums[d.back()]<=nums[i]){
-                d.pop_back();
-            }
-            d.push_back(i);
-            if(i>=k-1){
-                ans.push_back(nums[d.front()]);
+          if(!q.empty() && q.front() <= i - k){
+            q.pop_front();
+          }
+          while(!q.empty()&&nums[q.back()]<=nums[i]){
+            q.pop_back();
+          }
+          q.push_back(i);
+          if(i>=k-1){
+                ans.push_back(nums[q.front()]);
             }
         }
         return ans;
     }
 };
-//Final Algorithm Summary
-
-// Deque ko decreasing order mein maintain karo
-
-// Front hamesha max ka index rakhega
-
-// Har index ek baar add aur ek baar remove hota hai
