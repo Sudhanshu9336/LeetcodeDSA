49709c205addc8de37c0b65b15c47ae03ab0bb82
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-03-29 00:24:17 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (69.57%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+
+
+// "Agar dono frequency arrays same hain → substring permutation hai"
+// dekh sudhanshu mai kya karunga ki dono ka size nikala f1 mai maine pahle string ka freq har element ka dala 
+// for baad mai 0 se n size tak maine window banaya s2 ka freq dala compare kiya nahi hai then maine sliding window ko aage kiya i=n se badyaha jab tak i<m na ho jaye 
+// maine i=n wale ke fre ko ++ kiya aur [i-n] wale ka freq maine minus kiya compare karta raha  agar same mil jate hai toh mai return kar dunga true warna false he rahega na ashan hai yarr mere liye bahuut yeh question mai bhoolunga nahi iseee
+
+
     bool checkInclusion(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
