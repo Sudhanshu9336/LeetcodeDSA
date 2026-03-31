b8915fc60735aa8b1728fd39c279ab388d887d7b
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-03-31 10:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (32.56%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -10,47 +10,89 @@
  */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* temp=head;
-        //dekh sudhanhsu mai ja rah ahu pahle is list ka size pta karne ki kitna hai
-        if(head==NULL||head->next==NULL||k==0){
-            return head;
-        } 
-        int n=0;
-        while(temp){
-            n++;
-            temp=temp->next;
-        }
-        k=k%n;
-        if(k==0){
-            return head;
-        }
-        //ab mai ja raha hu naya listnode ki starting point search karne ki kha se start hoga ,peeche se n-k kar dunga uske peeche pauch jaunga dekho kaisse mai karne ja raha hu
-        int p=n-k;
-        ListNode* aagebadahao=head;
-        for(int i=1;i<p;i++){
-            aagebadahao=aagebadahao->next;
-        }
- ListNode* newstartinghead = aagebadahao->next;
-aagebadahao->next = NULL;
 
-//confusion yaha aati hai ki hamne kyu aisa kiya dekho  hamare pass
-//4->5->null aur 1->2->3 ab hame 4->5 ko head se jodna hai isliye ham 5 tak traverse karne wale end tak and in the last ham usse head se add kar dengey
-
-// **Find last node of rotated part**
-ListNode* last = newstartinghead;
-while(last->next!=NULL) {
-    last = last->next;
+ListNode* findlast(ListNode* head, int k){
+    while(head != NULL && k > 1){
+        head = head->next;
+        k--;
+    }
+    return head;
 }
-// **Link tail of rotated part back to original head**
-last->next = head;
+    ListNode* rotateRight(ListNode* head, int k) {
+                if(head == NULL || head->next == NULL || k == 0)
+            return head;
+        //ashan hai sabse dekho 
+        ListNode* tail=head;
+        int len=1;
+        while(tail->next!=NULL){
+            len++;
+            tail=tail->next;
+        }
+       k = k % len;
+        if(k == 0) return head;
+        tail->next=head;
+        ListNode* newlast=findlast(head,len-k);
+        ListNode* newhead=newlast->next;
+        newlast->next=NULL;
+          return newhead;
+    }
+  
+};
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* rotateRight(ListNode* head, int k) {
+//         ListNode* temp=head;
+//         //dekh sudhanhsu mai ja rah ahu pahle is list ka size pta karne ki kitna hai
+//         if(head==NULL||head->next==NULL||k==0){
+//             return head;
+//         } 
+//         int n=0;
+//         while(temp){
+//             n++;
+//             temp=temp->next;
+//         }
+        
+//         k=k%n;
+//         if(k==0){
+//             return head;
+//         }
+//         //ab mai ja raha hu naya listnode ki starting point search karne ki kha se start hoga ,peeche se n-k kar dunga uske peeche pauch jaunga dekho kaisse mai karne ja raha hu
+//         int p=n-k;
+//         ListNode* aagebadahao=head;
+//         for(int i=1;i<p;i++){
+//             aagebadahao=aagebadahao->next;
+//         }
+//  ListNode* newstartinghead = aagebadahao->next;
+// aagebadahao->next = NULL;
+
+// //confusion yaha aati hai ki hamne kyu aisa kiya dekho  hamare pass
+// //4->5->null aur 1->2->3 ab hame 4->5 ko head se jodna hai isliye ham 5 tak traverse karne wale end tak and in the last ham usse head se add kar dengey
+
+// // **Find last node of rotated part**
+// ListNode* last = newstartinghead;
+// while(last->next!=NULL) {
+//     last = last->next;
+// }
+// // **Link tail of rotated part back to original head**
+// last->next = head;
 
 
-return newstartinghead;
+// return newstartinghead;
 
         
-    }
-};
+//     }
+// };
 
 //we have done this by putting int the arra, then again in correct poistion of array then again building the linked list //we have created a new linked list by the element o the array taken on by one 
 
