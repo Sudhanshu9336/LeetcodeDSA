d419d00ec357d666e5f0fb2d1bbc878a8c45311e
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-04-02 21:45:02 +0530

    Time: 10 ms (80.61%) | Memory: 17.6 MB (14.98%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,24 +9,23 @@
  */
 class Solution {
 public:
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    if(root==NULL)return NULL;
-    if((root==p)||(root==q)){
+      if(root==NULL)return NULL;
+       if((root==p)||(root==q)){ //mai dekh raha hu ki root p ya q ke barabar hai toh wahi se return kar dunga wahi root
         return root;
-    }
-    TreeNode* left=lowestCommonAncestor(root->left,p,q);
-     TreeNode* right=lowestCommonAncestor(root->right,p,q);
-     if(left!=NULL && right!=NULL){
-        return root;
-     }
-      if(right!=NULL){
-        return right;
-     }
-     if(left!=NULL){
-        return left;
-     }
-     return NULL;
+         }
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);//left mai traverse karna hai jub tak mile na ya NUll na mile
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);//same
+        if(left!=NULL&&right!=NULL){  //maaan lo agr p and q dono left aur right mai mile matalb root hua 
+            return root;
+        }
+        if(right!=NULL){// left mai nahi mila sirf right mai mila toh right he returnn karunga sudhanhsu ji ok
+            return right;
+        }
+        if(left!=NULL){
+            return left;
+        }
+        return NULL;
 
     }
-    };
+};
