c19298aa25de8676d01e43495266e8b2af358874
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-04-02 14:04:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (85.17%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,19 +11,27 @@
  */
 class Solution {
 public:
-bool issafe(TreeNode* t1,TreeNode* t2){
- if(!t1  && !t2){
-    return true;
- }
- if(!t1||!t2){
-    return false;
- }
- return (t1->val==t2->val)&&issafe(t1->left,t2->right)&&issafe(t1->right,t2->left);
+
+bool issym(TreeNode* t1,TreeNode* t2){
+    if(t1==NULL&&t2==NULL){
+        return true;
+    }
+    if(t1==NULL||t2==NULL){
+        return false;
+    }
+
+    if(t1->val!=t2->val){
+        return false;
+    }
+    return issym(t1->left,t2->right)&&issym(t1->right,t2->left);
 }
     bool isSymmetric(TreeNode* root) {
         if(root==NULL){
             return true;
         }
-      return issafe(root->left,root->right);
+         TreeNode* left=root->left;
+         TreeNode* right=root->right;
+        return issym(left,right);
+
     }
 };
