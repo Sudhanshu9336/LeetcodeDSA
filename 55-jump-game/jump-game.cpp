0685c375febdbfb2582ebf2829bc07966ad7638c
Sudhanshu Gupta <126318520+Sudhanshu9336@users.noreply.github.com>
2026-04-13 02:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (56.52%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,14 +4,10 @@ public:
         int maxi=nums[0];
         int n=nums.size();
         for(int i=0;i<n;i++){
-           maxi=maxi-1;
-           maxi=max(maxi,nums[i]);
-           if(maxi==0){
-            if(i<n-1){
+            if(i>maxi){
                 return false;
             }
-        
-           }
+            maxi=max(maxi,nums[i]+i);
         }
         return true;
     }
